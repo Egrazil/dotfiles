da796694fa6e4550b99798a4ab8384b28c4d9bff
Egrazil <83728972+Egrazil@users.noreply.github.com>
2026-01-21 09:52:51 -0800

    add tabout and spider plugins

--- private_dot_config/nvim/lua/community.lua
+++ private_dot_config/nvim/lua/community.lua
@@ -20,4 +20,6 @@ return {
 
 	-- motions
 	{ import = "astrocommunity.motion.nvim-surround" },
+	{ import = "astrocommunity.motion.tabout-nvim" },
+	{ import = "astrocommunity.motion.nvim-spider" },
 }
